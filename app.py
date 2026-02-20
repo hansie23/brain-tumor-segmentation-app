e9d24acdfd77ef058c584971e60222051307790a
Hansie <63493237+hansie23@users.noreply.github.com>
2026-02-21 04:42:40 +0530

    Add repo_type parameter to model download function

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ def get_model(token):
     model_weights_path = hf_hub_download(
         repo_id="hansie23/brain-tumor-segmentation-model",
         filename="optimized_unet_checkpoint.keras",
+        repo_type="space",
         token=token
     )
     model = load_model_and_weights(model_weights_path)
